77e650c02e1ac87e741548e37f6adae985798d62
Firdavsi <firdavsinurov326@gmail.com>
2024-07-29 03:07:09 +0300

    🐶 test husky

--- src/shared/lib/cn.ts
+++ src/shared/lib/cn.ts
@@ -2,7 +2,8 @@ import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
+  // test
   const ad = 1
 
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
